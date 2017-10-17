60cefdc20e21e78f64093e7ef62e37395e0de54f
DavidRitch96 <david.ritch@mail.mcgill.ca>
2017-10-17 11:29:40 -0400

    Switched to iPhone app and character move
    Character moves without input now, next step will be making character
    loop back to beginning of screen

---
+++ New Unity Project/Assets/Scripts/LoopBack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopBack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if player reaches right end of screen
+		 * buffer for small amount of time
+		 * reappear on left side of screen
+		 */
+	}
+}
---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Player : MonoBehaviour {
+
+	public int movespeed = 1;
+	private Vector3 userDirection = Vector3.right;
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate(userDirection * movespeed * Time.deltaTime); 
+
+		/*if left side of screen touched
+		 * player jump
+		 * else if right side of screen is touched
+		 * player shoot
+		 */
+		
+	}
+}
